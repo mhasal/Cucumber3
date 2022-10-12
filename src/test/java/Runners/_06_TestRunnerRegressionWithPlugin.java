70873730dbe4d313b48d5990e22124686e7a77a2
Metin Hasal <metinhasal@outlook.com>
2022-10-12 21:19:24 +0300

    Jenkins için pom.xml e build eklendi

--- src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
+++ src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"},
 
-        plugin = { "html:target//cucumber-reports.html" }
+        plugin = { "html:target/cucumber-reports.html" }
         // mac için plugin = {"html:target/cucumber-reports.html"}
 )
 public class _06_TestRunnerRegressionWithPlugin extends AbstractTestNGCucumberTests {
